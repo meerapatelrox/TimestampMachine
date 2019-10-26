64968586deaea6f9a6f6e4b55e7cf4e6f51f1937
meerapatelrox <47922807+meerapatelrox@users.noreply.github.com>
2019-10-26 07:56:03 -0400

    Prints video ID in alert

--- popup.js
+++ popup.js
@@ -1,35 +1,34 @@
-/*** CONSTANTS ***/
-var DEFAULT_INSTANT_RESULTS = true;
-var ERROR_COLOR = '#ff8989';
-var WHITE_COLOR = '#ffffff';
-var ERROR_TEXT = "Content script was not loaded. Are you currently in the Chrome Web Store or in a chrome:// page? If you are, content scripts won't work here. If not, please wait for the page to finish loading or refresh the page.";
-var SHOW_HISTORY_TITLE = "Show search history";
-var HIDE_HISTORY_TITLE = "Hide search history";
-var ENABLE_CASE_INSENSITIVE_TITLE = "Enable case insensitive search";
-var DISABLE_CASE_INSENSITIVE_TITLE = "Disable case insensitive search";
-var HISTORY_IS_EMPTY_TEXT = "Search history is empty.";
-var CLEAR_ALL_HISTORY_TEXT = "Clear History";
-var DEFAULT_CASE_INSENSITIVE = false;
-var MAX_HISTORY_LENGTH = 30;
-/*** CONSTANTS ***/
+/*** GETS VIDEO ID FROM URL ***/
+function getVideoID(aVid) {
+  /* Gets URL */
+  chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
+    function(tabs){
+      var curr_url = tabs[0].url;
+      //alert(curr_url); // works!
+      
+      /* Gets video ID */
+      aVid.ID = getUrlVars(curr_url)["v"];
+      alert(aVid.ID);
+    }
+  );
+  }
 
-/*** VARIABLES ***/
-var sentInput = false;
-var processingKey = false;
-var searchHistory = null;
-var maxHistoryLength = MAX_HISTORY_LENGTH;
-/*** VARIABLES ***/
+  function getUrlVars(url) {
+    var vars = {};
+    var parts = url.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+var myVid = {
+  ID: ""
+};
+
+getVideoID(myVid);
+//alert(myVid.ID);
 
 /*** FUNCTIONS ***/
-/* Validate that a given pattern string is a valid regex */
-function isValidRegex(pattern) {
-  try{
-    var regex = new RegExp(pattern);
-    return true;
-  } catch(e) {
-    return false;
-  }
-}
 
 /* Send message to content script of tab to select next result */
 function selectNext(){
@@ -62,9 +61,10 @@ function selectPrev(){
 }
 
 /* Send message to pass input string to content script of tab to find and highlight regex matches */
-function passInputToContentScript(){
+
+/* function passInputToContentScript(){
   passInputToContentScript(false);
-}
+} */
 
 /*** LISTENERS ***/
 document.getElementById('next').addEventListener('click', function() {
@@ -77,9 +77,9 @@ document.getElementById('prev').addEventListener('click', function() {
 
 document.getElementById('clear').addEventListener('click', function() {
   sentInput = false;
-  document.getElementById('inputRegex').value = '';
+  document.getElementById('userInput').value = '';
   passInputToContentScript();
-  document.getElementById('inputRegex').focus();
+  document.getElementById('userInput').focus();
 });
 
 /* Received returnSearchInfo message, populate popup UI */ 
@@ -92,12 +92,12 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
       document.getElementById('numResults').textContent = String(request.currentSelection) + ' of ' + String(request.numResults);
     }
     if (!sentInput) {
-      document.getElementById('inputRegex').value = request.regexString;
+      document.getElementById('userInput').value = request.regexString;
     }
     if (request.numResults > 0 && request.cause == 'selectNode') {
       addToHistory(request.regexString);
     }
-    if (request.regexString !== document.getElementById('inputRegex').value) {
+    if (request.regexString !== document.getElementById('userInput').value) {
       passInputToContentScript();
     }
   }
@@ -109,7 +109,7 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 var map = [];
 onkeydown = onkeyup = function(e) {
     map[e.keyCode] = e.type == 'keydown';
-    if (document.getElementById('inputRegex') === document.activeElement) { //input element is in focus
+    if (document.getElementById('userInput') === document.activeElement) { //input element is in focus
       if (!map[16] && map[13]) { //ENTER
         if (sentInput) {
           selectNext();
@@ -132,11 +132,11 @@ chrome.storage.local.get({
     'isSearchHistoryVisible' : false},
   function(result) {
     if(result.instantResults) {
-      document.getElementById('inputRegex').addEventListener('input', function() {
+      document.getElementById('userInput').addEventListener('input', function() {
         passInputToContentScript();
       });
     } else {
-      document.getElementById('inputRegex').addEventListener('change', function() {
+      document.getElementById('userInput').addEventListener('change', function() {
         passInputToContentScript();
       });
     }
@@ -176,9 +176,9 @@ function(tabs) {
 });
 
 /* Focus onto input form */
-document.getElementById('inputRegex').focus();
+document.getElementById('userInput').focus();
 window.setTimeout( 
-  function(){document.getElementById('inputRegex').select();}, 0);
+  function(){document.getElementById('userInput').select();}, 0);
 //Thanks to http://stackoverflow.com/questions/480735#comment40578284_14573552
 
 var makeVisible = document.getElementById('history').style.display == 'none';
